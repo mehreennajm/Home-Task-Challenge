2e09c0d9b96a95d6282428cc5a19e9b2c206d061
Mehreen Najm <31657799+mehreennajm@users.noreply.github.com>
2022-07-02 13:34:41 +0200

    updated favicon

--- resources/views/login-layout/layout.blade.php
+++ resources/views/login-layout/layout.blade.php
@@ -3,6 +3,8 @@
 
 <head>
     <title>Pigment Product Management System |  @yield('title')</title>
+    <!-- Favicon icon -->
+     <link rel="icon" href="{{asset('assets/images/favicon.ico')}}" type="image/x-icon">
     <!-- Meta -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
